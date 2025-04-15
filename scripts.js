52c835222c59ce46bd19ab60c553f6d7537b8abf
Jhonier1227 <maily0877@gmail.com>
2025-04-15 09:51:14 -0500

    le agregue mas diseño luego continuo

--- scripts.js
+++ scripts.js
@@ -7,3 +7,13 @@ function copiarCorreo() {
     // Confirmación visual
     alert("Correo copiado: " + inputCorreo.value);
   }
+
+  window.addEventListener("scroll", function () {
+    const navbar = document.querySelector(".navbar");
+    if (window.scrollY > 50) {
+      navbar.classList.add("navbar-scrolled");
+    } else {
+      navbar.classList.remove("navbar-scrolled");
+    }
+  });
+  
